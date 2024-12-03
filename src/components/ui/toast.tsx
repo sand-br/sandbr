c88b1f4a034b43559abd5bb3f821badc332d7919
raave-aires <raave.aires@proton.me>
2024-12-03 01:22:29 -0300

    style: estilos do toast e do tooltip ajustados

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border-2 bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
