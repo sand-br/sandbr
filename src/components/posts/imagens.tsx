6dbf3f0875364a6203f0718a044888cea112e637
raave-aires <raave.aires@proton.me>
2024-12-03 12:05:06 -0300

    feat: componente de imagem que alterna com o tema

--- src/components/posts/imagens.tsx
+++ src/components/posts/imagens.tsx
@@ -1,6 +1,6 @@
 // importação de dependências:
 import React, { ReactNode } from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 //importação de componentes:
 import { Ir } from "./ligacoes";
@@ -12,13 +12,26 @@ import {
 } from "@/src/components/ui/tooltip";
 
 interface Container {
-  children: ReactNode,
-  link_do_artista?: string,
-  artista?: string,
-  descricao: string, 
+  children: ReactNode;
+  link_do_artista?: string;
+  artista?: string;
+  descricao: string;
 }
 
-function ContainerDeImagem({ children, link_do_artista, artista, descricao }: Container){
+interface Imagens {
+  claro: string | StaticImageData;
+  escuro: string | StaticImageData;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+function ContainerDeImagem({
+  children,
+  link_do_artista,
+  artista,
+  descricao,
+}: Container) {
   const credito = () => {
     return (
       <>
@@ -38,19 +51,46 @@ function ContainerDeImagem({ children, link_do_artista, artista, descricao }: Co
     );
   };
 
-  return(
+  return (
     <div className="border border-border flex flex-col items-center mx-5 my-3 gap-2 p-2 rounded-md sm:max-w-[300px] md:max-w-[400px]">
-      <div className="rounded-md overflow-hidden">
-        {children}
-      </div>
+      <div className="rounded-md overflow-hidden">{children}</div>
 
       {artista == undefined ? null : (
         <p className="text-xs text-citacao">Feita por {credito()}.</p>
       )}
       <p className="text-xs text-citacao">{descricao}</p>
     </div>
-
-  ) 
+  );
 }
 
-export { ContainerDeImagem };
+const ImagemTematica: React.FC<Imagens> = ({
+  claro,
+  escuro,
+  alt,
+  width,
+  height,
+}) => {
+  return (
+    <>
+      {/*esta imagem será exibida quando o tema claro estiver ativo*/}
+      <Image
+        src={claro}
+        alt={alt}
+        width={width}
+        height={height}
+        className="block dark:hidden"
+      />
+
+      {/*esta imagem será exibida quando o tema escuro estiver ativo*/}
+      <Image
+        src={escuro}
+        alt={alt}
+        width={width}
+        height={height}
+        className="hidden dark:block"
+      />
+    </>
+  );
+};
+
+export { ContainerDeImagem, ImagemTematica };
