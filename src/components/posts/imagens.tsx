0ce59d50b842b3a46ccfd954839995c478c878c3
raave-aires <raave.aires@proton.me>
2024-12-04 15:59:54 -0300

    feat: ContainerDeImagem agora tem um Image dentr. style: tamanho agora é definido via estilos

--- src/components/posts/imagens.tsx
+++ src/components/posts/imagens.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 // importação de dependências:
 import React, { ReactNode } from "react";
 import Image, { StaticImageData } from "next/image";
@@ -12,25 +14,25 @@ import {
 } from "@/src/components/ui/tooltip";
 
 interface Container {
-  children: ReactNode;
   link_do_artista?: string;
   artista?: string;
+  src: string | StaticImageData;
   descricao: string;
 }
 
 interface Imagens {
   claro: string | StaticImageData;
   escuro: string | StaticImageData;
-  alt: string;
-  width: number;
-  height: number;
+  link_do_artista?: string;
+  artista?: string;
+  descricao: string;
 }
 
 function ContainerDeImagem({
-  children,
   link_do_artista,
   artista,
   descricao,
+  src,
 }: Container) {
   const credito = () => {
     return (
@@ -52,45 +54,68 @@ function ContainerDeImagem({
   };
 
   return (
-    <div className="border border-border flex flex-col items-center mx-5 my-3 gap-2 p-2 rounded-md sm:max-w-[300px] md:max-w-[400px]">
-      <div className="rounded-md overflow-hidden">{children}</div>
+    <div className="border border-border flex flex-col items-center mx-5 my-3 lg:my-0 mb-3 gap-2 p-2 rounded-md sm:max-w-[300px] md:max-w-[500px]">
+      <div className="rounded-md overflow-hidden">
+        <Image src={src} width={500} height={500} alt={descricao}/>
+      </div>
 
       {artista == undefined ? null : (
         <p className="text-xs text-citacao">Feita por {credito()}.</p>
       )}
-      <p className="text-xs text-citacao">{descricao}</p>
+
+      <p className="text-xs text-citacao text-center">{descricao}</p>
     </div>
   );
 }
 
-const ImagemTematica: React.FC<Imagens> = ({
-  claro,
-  escuro,
-  alt,
-  width,
-  height,
-}) => {
+const ImagemTematica: React.FC<Imagens> = ({ claro, escuro, link_do_artista, artista, descricao }) => {
+  const credito = () => {
+    return (
+      <>
+        {link_do_artista == undefined ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>{artista}</TooltipTrigger>
+              <TooltipContent>
+                <p>Não encontramos um perfil oficial deste artista.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ) : (
+          <Ir destino={link_do_artista!} texto={artista!} />
+        )}
+      </>
+    );
+  };
+
   return (
-    <>
-      {/*esta imagem será exibida quando o tema claro estiver ativo*/}
-      <Image
-        src={claro}
-        alt={alt}
-        width={width}
-        height={height}
-        className="block dark:hidden"
-      />
+    <div className="border border-border flex flex-col items-center mx-5 my-3 lg:my-0 mb-3 gap-2 p-4 rounded-md sm:max-w-[300px] md:max-w-[500px]">
+      <div className="rounded-md overflow-hidden">
+        {/*esta imagem será exibida quando o tema claro estiver ativo*/}
+        <Image
+          src={claro}
+          alt={descricao}
+          width={500}
+          height={500}
+          className="block dark:hidden"
+        />
 
-      {/*esta imagem será exibida quando o tema escuro estiver ativo*/}
-      <Image
-        src={escuro}
-        alt={alt}
-        width={width}
-        height={height}
-        className="hidden dark:block"
-      />
-    </>
+        {/*esta imagem será exibida quando o tema escuro estiver ativo*/}
+        <Image
+          src={escuro}
+          alt={descricao}
+          width={500}
+          height={500}
+          className="hidden dark:block"
+        />
+      </div>
+
+      {artista == undefined ? null : (
+        <p className="text-xs text-citacao">Feita por {credito()}.</p>
+      )}
+
+      <p className="text-xs text-citacao text-center">{descricao}</p>
+    </div>
   );
-};
-
+}
 export { ContainerDeImagem, ImagemTematica };
