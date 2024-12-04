4406580250e964643c7feca0db1fd35972c12404
raave-aires <raave.aires@proton.me>
2024-12-04 15:56:41 -0300

    style: text-zinc-400 substituido pelo text-citacao, adaptado para nova interface com temas

--- src/components/posts/ligacoes.tsx
+++ src/components/posts/ligacoes.tsx
@@ -10,8 +10,8 @@ const estilo_de_link = "text-destaque hover:underline scroll-mt-[84px]"
 
 // lista de tipagem:
 interface Ancoras {
-    o_que: string;
-    titulo: string;
+  o_que: string;
+  titulo: string;
 }
 
 interface Menção {
@@ -19,76 +19,76 @@ interface Menção {
 }
 
 interface Enlaces {
-    destino: string;
-    texto: string;
-  }
+  destino: string;
+  texto: string;
+}
 
 interface Listagem {
-    qual: string,
+  qual: string,
 }
 
 interface AncReferencias {
-    qual: number,
+  qual: number,
 }
 
 interface Referencias {
-    qual: number,
-    children: string | ReactNode
+  qual: number,
+  children: string | ReactNode
 }
 
 // Ancorar é usado para criar links entre seções de uma mesma página
 const Ancorar: React.FC<Ancoras> = ({ o_que, titulo }) => {
-    return (
-        <Link href={`#${o_que}`} className={estilo_de_link} scroll={true}>
-          {titulo}
-        </Link>
-    );
+  return (
+    <Link href={`#${o_que}`} className={estilo_de_link} scroll={true}>
+      {titulo}
+    </Link>
+  );
 };
 
 // Enlaçar é usado para criar links para outras páginas dentro da enciclopédia
 const Enlaçar: React.FC<Enlaces> = ({ destino, texto }) => {
   return (
-      <Link href={destino} className={estilo_de_link} prefetch={true}>
-        {texto}
-      </Link>
+    <Link href={destino} className={estilo_de_link} prefetch={true}>
+      {texto}
+    </Link>
   );
 };
 
 // Listar é usado para mencionar os livros em uma página, e criará um link para a página da enciclopédia do livro em questão
-const Listar: React.FC<Listagem> = ({qual}) => {
-    return(
-      <Link className={estilo_de_link} href={`/livros/${qual.toLowerCase().replace(/\s+/g, '-')}`}>{qual}</Link>
-    )
-  };
+const Listar: React.FC<Listagem> = ({ qual }) => {
+  return (
+    <Link className={estilo_de_link} href={`/livros/${qual.toLowerCase().replace(/\s+/g, '-')}`}>{qual}</Link>
+  )
+};
 
 // Referenciar é usado para criar uma âncora que leva à área de referências
-const Referenciar: React.FC<AncReferencias> = ({qual}) => {
-    return (
-      <sup className="scroll-mt-[84px]">
-        <Link href={`#nota_${qual}`} className={estilo_de_link} id={`nota_rev_${qual}`}>{`[${qual}]`}</Link>
-      </sup>
-    );
+const Referenciar: React.FC<AncReferencias> = ({ qual }) => {
+  return (
+    <sup className="scroll-mt-[84px]">
+      <Link href={`#nota_${qual}`} className={estilo_de_link} id={`nota_rev_${qual}`}>{`[${qual}]`}</Link>
+    </sup>
+  );
 }
 
 // CriarReferencia usado para criar uma referência e uma âncora que leva de volta ao conteúdo ao qual a refêrencia se relaciona.
-const CriarReferencia: React.FC<Referencias> = ({qual, children}) => {
-    return (
-        <div className="flex gap-3">
-            <Link href={`#nota_rev_${qual}`} className="text-zinc-400 hover:text-destaque hover:underline"><ArrowUp size={16}/></Link>
-            <p className="text-xs text-zinc-400" id={`nota_${qual}`}>{`[${qual}]`} {children}</p>
-        </div>
-        
-    );
+const CriarReferencia: React.FC<Referencias> = ({ qual, children }) => {
+  return (
+    <div className="flex gap-3">
+      <Link href={`#nota_rev_${qual}`} className="text-citacao hover:text-destaque hover:underline"><ArrowUp size={16} /></Link>
+      <p className="text-xs text-citacao" id={`nota_${qual}`}>{`[${qual}]`} {children}</p>
+    </div>
+
+  );
 }
 
 // Ir é usado para linkar um site fora da enciclopédia
-const Ir: React.FC<Enlaces> = ({destino, texto}) =>{
+const Ir: React.FC<Enlaces> = ({ destino, texto }) => {
   return <a href={destino} target="_blank" rel="noopener noreferrer" className={estilo_de_link}>{texto}</a>
 }
 
 // Mencionar é usado para citar um personagem
 const Mencionar: React.FC<Menção> = ({ quem }) => {
-  return(
+  return (
     <Link className="text-destaque hover:underline" href={`/personagens/${quem.toLowerCase().replace(/[' ]/g, '-')}`}>{quem}</Link>
   )
 };
