75f5562ed4b69a47d65290213d56b5f31aadfa7e
raave-aires <raave.aires@proton.me>
2024-12-04 20:59:48 -0300

    feat: componente de imagens fragmentado

--- src/components/imagens/imagens-tematica.tsx
+++ src/components/imagens/imagens-tematica.tsx
@@ -5,20 +5,13 @@ import React from "react";
 import Image, { StaticImageData } from "next/image";
 
 //importação de componentes:
-import { Ir } from "./ligacoes";
+import { Ir } from "../posts/ligacoes";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/src/components/ui/tooltip";
-
-interface Container {
-  link_do_artista?: string;
-  artista?: string;
-  src: string | StaticImageData;
-  descricao: string;
-}
 
 interface Imagens {
   claro: string | StaticImageData;
@@ -28,47 +21,7 @@ interface Imagens {
   descricao: string;
 }
 
-function ContainerDeImagem({
-  link_do_artista,
-  artista,
-  descricao,
-  src,
-}: Container) {
-  const credito = () => {
-    return (
-      <>
-        {link_do_artista == undefined ? (
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>{artista}</TooltipTrigger>
-              <TooltipContent>
-                <p>Não encontramos um perfil oficial deste artista.</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        ) : (
-          <Ir destino={link_do_artista!} texto={artista!} />
-        )}
-      </>
-    );
-  };
-
-  return (
-    <div className="border border-border flex flex-col items-center mx-5 my-3 lg:my-0 mb-3 gap-2 p-2 rounded-md sm:max-w-[300px] md:max-w-[500px]">
-      <div className="rounded-md overflow-hidden">
-        <Image src={src} width={500} height={500} alt={descricao}/>
-      </div>
-
-      {artista == undefined ? null : (
-        <p className="text-xs text-citacao">Feita por {credito()}.</p>
-      )}
-
-      <p className="text-xs text-citacao text-center">{descricao}</p>
-    </div>
-  );
-}
-
-const ImagemTematica: React.FC<Imagens> = ({ claro, escuro, link_do_artista, artista, descricao }) => {
+export const ImagemTematica: React.FC<Imagens> = ({ claro, escuro, link_do_artista, artista, descricao }) => {
   const credito = () => {
     return (
       <>
@@ -118,4 +71,3 @@ const ImagemTematica: React.FC<Imagens> = ({ claro, escuro, link_do_artista, art
     </div>
   );
 }
-export { ContainerDeImagem, ImagemTematica };
