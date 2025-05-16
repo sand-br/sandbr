8cd1710477d7781633673ba966289bcf6cd87cf3
Piblio <dev@raave.me>
2025-05-16 17:17:40 -0300

    chore[folds]: atualiza a organização das pastas

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({
     <div className="flex flex-col">
       <SiteHeader />
       <div className="flex justify-center shrink-0">
-        <main className="min-h-dvh w-2/3 flex flex-col  bg-amber-800 ">
+        <main className="min-h-dvh w-2/3 flex flex-col">
           {children}
         </main>
       </div>
