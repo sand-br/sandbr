d988e9f7383460576e3515102aa324c8088bcf5d
Piblio <dev@raave.me>
2025-05-16 16:16:24 -0300

    feat(artigo): cria o layout das páginas de criação e edição de artigos

---
+++ app/artigo/layout.tsx
@@ -0,0 +1,18 @@
+import { SiteHeader } from "@/components/site-header";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return(
+    <div className="flex flex-col">
+      <SiteHeader />
+      <div className="flex justify-center shrink-0">
+        <main className="min-h-dvh w-2/3 flex flex-col  bg-amber-800 ">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
