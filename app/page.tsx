3291cde1f3ba0bc08037cdf31242404fbd69920e
Sandbr <equipe@sandbr.org>
2025-05-23 18:48:22 +0000

    wip: novas imagens adicionadas ao deslizante, footer com copyright

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ import {
   DrawerClose,
   DrawerContent,
   DrawerTrigger,
-} from "@/components/ui/drawer"
+} from "@/components/ui/drawer";
 import { Marquee } from "@/components/magicui/marquee";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/ui/tooltip";
 import { WordRotate } from "@/components/magicui/word-rotate";
 
 // importação de ícones:
@@ -25,124 +25,225 @@ import { MoveRight } from "lucide-react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 // importação de arquivos:
-import Kaladin from "@/public/imgs/kaladin.webp"
-import Kelsier from "@/public/imgs/kelsier.jpg"
-import Siri from "@/public/imgs/siri.jpg"
-import Szeth from "@/public/imgs/szeth.jpg"
-import Tress from "@/public/imgs/tress-e-charlie.jpg"
-import Vasher from "@/public/imgs/vasher.png"
-import Vin from "@/public/imgs/vin.jpg"
+import Hoid from "@/public/home_imgs/hoid.jpg";
+import Ishikk from "@/public/home_imgs/ishikk.jpg";
+import Kaladin from "@/public/home_imgs/kaladin.webp";
+import Licenses from "@/public/licenças.svg"
+import Kelsier from "@/public/home_imgs/kelsier.jpg";
+import Siri from "@/public/home_imgs/siri.jpg";
+import Szeth from "@/public/home_imgs/szeth.jpg";
+import Tress from "@/public/home_imgs/tress-e-charlie.jpg";
+import Vasher from "@/public/home_imgs/vasher.png";
+import Vin from "@/public/home_imgs/vin.jpg";
+
+import { Infos, CopyrightTable } from "@/components/copyright-table";
+import { Logo } from "@/components/logo";
+import { CopyrightAndLicences } from "@/components/copyright-with-year";
 
 export default function Home() {
-  const palavras = ["Brandon Sanderson.", "o Cosmere.", "Nascidos da Bruma.", "Os relatos da Guerra das Tempestades.", "Warbreaker - O sopro dos deuses.","Tress, a Garota do Mar Esmeralda.","Executores.","Elantris.","o Hoid."]
+  const palavras = [
+    "Brandon Sanderson.",
+    "o Cosmere.",
+    "Nascidos da Bruma.",
+    "Os relatos da Guerra das Tempestades.",
+    "Warbreaker - O sopro dos deuses.",
+    "Tress, a Garota do Mar Esmeralda.",
+    "Executores.",
+    "Elantris.",
+    "o Hoid.",
+  ];
+
+  const creditosImagens: Infos[] = [
+    {
+      imagem: "Hoid, 'Worldsinger'",
+      autor: "Ari Ibarra, Brotherwise Games",
+    },
+    {
+      imagem: "Kelsier",
+      autor: "Miranda Meeks, Dragonssteel Entertainment",
+    },
+    {
+      imagem: "Szeth, 'Weep As You Kill'",
+      autor: "Ari Ibarra, Brotherwise Games",
+    },
+    {
+      imagem: "Tress e Charlie, 'Na parede baixa do jardim'",
+      autor: "Howard Lyon, Dragonssteel Entertainment",
+    },
+    {
+      imagem:
+        "Vasher, 'Running through the God King's palace wielding Nightblood'",
+      autor: "Esther Schrader",
+    },
+    {
+      imagem: "Vin",
+      autor: "Deandra Scicluna",
+    },
+    
+  ];
 
   return (
     <section>
       <div className="w-full select-none border-b">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-2">
+            {" "}
+            {/* esta div comporta os elementos visuais principais do início */}
             <div className="flex gap-4 flex-col lg:pl-12">
+              {" "}
+              {/* esta div comporta a saudação de boas-vindas e seus elementos complementares*/}
               <div>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger className="cursor-help hidden lg:block">
+                {" "}
+                {/* esta é a div que contém o badge e o drawer de aviso de spoilers */}
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className="cursor-help hidden lg:block">
+                      <Badge variant="destructive">Alerta de spoilers!</Badge>
+                    </TooltipTrigger>
+                    <TooltipContent className="w-80 bg-background border text-white hyphens-auto ml-4">
+                      <p>
+                        O conteúdo desta enciclopédia contém revelações
+                        significativas sobre todas as obras do Brandon
+                        Sanderson. Leia por sua conta e risco.
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <Drawer>
+                  <DrawerTrigger className="lg:hidden">
                     <Badge variant="destructive">Alerta de spoilers!</Badge>
-                  </TooltipTrigger>
-                  <TooltipContent className="w-80 bg-background border text-white hyphens-auto ml-4">
-                    <p>O conteúdo desta enciclopédia contém revelações significativas sobre todas as obras do Brandon Sanderson. Leia por sua conta e risco.</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              <Drawer>
-                <DrawerTrigger className="lg:hidden">
-                  <Badge variant="destructive">Alerta de spoilers!</Badge>
-                </DrawerTrigger>
-                <DrawerContent className="mb-10">
-                  <p className="m-8 hyphens-auto text-center">O conteúdo desta enciclopédia contém revelações significativas sobre todas as obras do Brandon Sanderson. Leia por sua conta e risco.</p>
-                  <DrawerClose>
-                    <Button>Estou ciente</Button>
-                  </DrawerClose>
-                </DrawerContent>
-              </Drawer>
+                  </DrawerTrigger>
+                  <DrawerContent className="mb-10">
+                    <p className="m-8 hyphens-auto text-center">
+                      O conteúdo desta enciclopédia contém revelações
+                      significativas sobre todas as obras do Brandon Sanderson.
+                      Leia por sua conta e risco.
+                    </p>
+                    <DrawerClose>
+                      <Button>Estou ciente</Button>
+                    </DrawerClose>
+                  </DrawerContent>
+                </Drawer>
               </div>
-              
               <div className="flex gap-4 flex-col">
                 <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-regular">
-                  bem-vindo(a) à <span className="text-[#e73720]">sandbr</span>!
+                  bem-vindo(a) à{" "}
+                  <span className="text-[#e73720] font-averia">sandbr</span>!
                 </h1>
                 <p className="text-xl tracking-tight text-muted-foreground max-w-md text-left leading-none">
-                  A enciclopédia brasileira sobre <WordRotate words={palavras.sort(() => Math.random() - 0.5)} duration={5000}/>
+                  A enciclopédia brasileira sobre{" "}
+                  <WordRotate
+                    words={palavras.sort(() => Math.random() - 0.5)}
+                    duration={5000}
+                  />
                 </p>
               </div>
               <div className="flex flex-row gap-4">
                 <Button size="lg" className="gap-4" variant="outline" asChild>
-                  <a href="https://github.com/sand-br/sandbr" target="_blank" rel="noopener noreferrer">Contribua <GitHubLogoIcon className="w-4 h-4" /></a>
+                  <a
+                    href="https://github.com/sand-br/sandbr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Contribua <GitHubLogoIcon className="size-4" />
+                  </a>
                 </Button>
                 <Button size="lg" asChild>
-                  <Link href="/assuntos" className="gap-4">Explorar <MoveRight className="w-4 h-4" /></Link>
+                  <Link href="/assuntos" className="gap-4">
+                    Explorar <MoveRight className="w-4 h-4" />
+                  </Link>
                 </Button>
               </div>
             </div>
-
             <div className="relative flex max-h-dvh items-center justify-center overflow-hidden">
+              {" "}
+              {/* esta div comporta o marquee de imagens */}
               <Marquee vertical pauseOnHover={true} className="max-h-dvh">
-                  <Image
-                    src={Vin}
-                    alt="Vin"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
-                  <Image
-                    src={Siri}
-                    alt="Siri"
-                    className="rounded-lg object-cover aspect-square"
-                    width={300}
-                    
-                  />
-                  <Image
-                    src={Kaladin}
-                    alt="Kaladin"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
-                   <Image
-                    src={Kelsier}
-                    alt="Kelsier"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
+                <Image
+                  src={Hoid}
+                  alt="Kelsier"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+                <Image
+                  src={Vin}
+                  alt="Vin"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+                <Image
+                  src={Siri}
+                  alt="Siri"
+                  className="rounded-lg object-cover aspect-square"
+                  width={300}
+                />
+                <Image
+                  src={Kaladin}
+                  alt="Kaladin"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+                <Image
+                  src={Kelsier}
+                  alt="Kelsier"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
               </Marquee>
-              <Marquee vertical pauseOnHover={true} reverse className="max-h-dvh">
-                  <Image
-                    src={Tress}
-                    alt="Tress"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
-                  <Image
-                    src={Szeth}
-                    alt="Szeth"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
-                  <Image
-                    src={Vasher}
-                    alt="Vasher"
-                    className="rounded-lg object-cover"
-                    width={300}
-                    height={300}
-                  />
+              <Marquee
+                vertical
+                pauseOnHover={true}
+                reverse
+                className="max-h-dvh"
+              >
+                <Image
+                  src={Ishikk}
+                  alt="Kelsier"
+                  className="rounded-lg object-cover aspect-video"
+                  width={300}
+                />
+                <Image
+                  src={Tress}
+                  alt="Tress"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+                <Image
+                  src={Szeth}
+                  alt="Szeth"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+                <Image
+                  src={Vasher}
+                  alt="Vasher"
+                  className="rounded-lg object-cover"
+                  width={300}
+                  height={300}
+                />
               </Marquee>
               <div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div>
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div>
+              <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div>
             </div>
           </div>
+
+          <footer className="grid grid-cols-2 border-t py-8">
+            <div className="pr-8">
+              <Logo />
+              <CopyrightAndLicences />
+              <Image src={Licenses} alt="Selos de licenças da SandBR" width={250} className="h-full mt-auto"/>
+            </div>
+            <div className="flex flex-col items-center justify-center max-h-[25rem]">
+              <p className="self-start pb-4">Atribuição de créditos:</p>
+              <CopyrightTable info={creditosImagens} />
+            </div>
+          </footer>
         </div>
       </div>
     </section>
