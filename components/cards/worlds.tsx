68bc25338cd7e3b84ccbcb9e068b60a3ae894a77
Raave Aires <dev@raave.me>
2025-05-25 14:43:28 -0300

    wip[worlds]: cria o componente worlds

---
+++ components/cards/worlds.tsx
@@ -0,0 +1,12 @@
+export function Worlds() {
+  return (
+    <div className="rounded-lg border justify-center items-center px-6">
+      <div className="flex items-center justify-between py-4">
+        <p className="font-semibold">Mundos</p>
+      </div>
+      <div className="min-w-dvh flex flex-wrap gap-4 border-t py-8">
+        
+      </div>
+    </div>
+  );
+}
