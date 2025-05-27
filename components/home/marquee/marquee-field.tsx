be47c6484be7bc9f0fb05b65aac3e4bffb88e100
Raave Aires <dev@raave.me>
2025-05-26 21:42:10 -0300

    feat: remove import desnecessário do utilitário cn no componente marquee-field

--- components/home/marquee/marquee-field.tsx
+++ components/home/marquee/marquee-field.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { cn } from "@/lib/utils";
 
 import { Marquee } from "@/components/magicui/marquee";
 import Image, { StaticImageData } from "next/image";
