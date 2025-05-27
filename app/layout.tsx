f07396900bfa2511843e408332ef005835688bdf
Raave Aires <dev@raave.me>
2025-05-27 19:42:10 -0300

    feat: adiciona a propriedade display "swap" aos componentes de fonte

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,17 +20,20 @@ const averia = Averia_Serif_Libre({
   weight: "400",
   style: "normal",
   variable: "--font-averia",
-  subsets: ["latin"]
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
